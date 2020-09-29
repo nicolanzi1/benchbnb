087ef5142ebe9f50e9e5a0c3d96ba1f36056eba2
nicolanzi1 <nicolanzi@gmail.com>
2020-09-29 16:08:25 +0100

    Wrote React Router, Session Components and Rails User ans session model and controllers

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,5 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
     window.dispatch = store.dispatch;
 
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to BenchBnb</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
---
+++ frontend/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Greeting from "./greeting/greeting";
+import {
+    Route,
+} from 'react-router-dom'
+
+import GreetingContainer from './greeting/greeting_container';
+import SignupFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
+
+const App = () => (
+    <div>
+        <header>
+        <h1>Bench BnB</h1>
+        <GreetingContainer />
+        </header>
+
+        <Route path="/login" component={LoginFormContainer} />
+        <Route path="/signup" component={SignupFormContainer} />
+    </div>
+);
+
+export default App;
