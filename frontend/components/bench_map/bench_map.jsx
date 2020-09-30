c2707fe22b35502697536c67fc40ed8d3b748349
nicolanzi1 <nicolanzi@gmail.com>
2020-09-30 21:18:21 +0100

    Wrote map markers for the benches with a MarkerManager helper method inside utils

--- frontend/components/bench_map/bench_map.jsx
+++ frontend/components/bench_map/bench_map.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import MarkerManager from '../../util/marker_manager';
+
 const mapOptions ={
     center: { lat: 53.480759, lng: -2.242631 },
     zoom: 13
@@ -9,7 +11,13 @@ const mapOptions ={
 class BenchMap extends Component {
     componentDidMount() {
         const map = this.refs.map;
-        this.map = new google.maps.Map(this.mapNode, mapOptions);
+        this.map = new google.maps.Map(mapDOMNode, mapOptions);
+        this.MarkerManager = new MarkerManager(this.map);
+        this.MarkerManager.updateMarkers(this.props.benches);
+    }
+
+    componentDidUpdate() {
+        this.MarkerManager.updateMarkers(this.props.benches);
     }
 
     render() {
---
+++ frontend/util/marker_manager.js
@@ -0,0 +1,21 @@
+export default class MarkerManager {
+    constructor(map) {
+        this.map = map;
+        this.markers = {};
+    }
+
+    updateMarkers(benches) {
+        console.log("time to update");
+    }
+
+    createMarkerFromBench(bench) {
+        const position = new google.maps.LatLng(bench.lat, bench.lng);
+        const marker = new google.maps.Marker({
+            position,
+            map: this.map,
+            benchId: bench.id
+        });
+
+        this.markers[marker.benchId] = marker;
+    }
+}
